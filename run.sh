cebbdf43c55f62539429c8f9cf12a896f8e8167d
J.R. Patten <jr@juanpatten.com>
2013-11-08 18:45:26 -0700

    Add updated config for latest kibana

--- run.sh
+++ run.sh
@@ -2,16 +2,14 @@
 ES_HOST=${ES_HOST:-window.location.hostname}
 ES_PORT=${ES_PORT:-9200}
 
-cat << EOF > /usr/share/nginx/www/config.js
-var config = new Settings(
-{
-  elasticsearch:    "http://$ES_HOST:$ES_PORT",
-  kibana_index:     "kibana-int",
-  modules:          ['histogram','map','pie','table','stringquery','sort',
-                    'timepicker','text','fields','hits','dashcontrol',
-                    'column','derivequeries','trends','bettermap'],
-  }
-);
-EOF
+CONFIG_FILE="/usr/share/nginx/www"
+
+function config_replace() {
+    CMD="s/{{$1}}/$2/g"
+    sed -i'.bak' -e ${CMD} ${CONFIG_FILE}
+}
+
+config_replace "ES_HOST" "$ES_HOST"
+config_replace "ES_PORT" "$ES_PORT"
 
 nginx -c /etc/nginx/nginx.conf
